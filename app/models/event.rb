af433ad62bc89efcce8a7d49b1d22021a928bf57
AFoo97 <arthur.foozb@gmail.com>
2025-07-31 10:25:42 +1000

    Add event, event attendance, and user associations with scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,13 @@
 class Event < ApplicationRecord
+  belongs_to :creator, class_name: 'User'
+
+  has_many :event_attendances, foreign_key: :attended_event_id, dependent: :destroy
+  has_many :attendees, through: :event_attendances, source: :attendee
+
+  has_many :invitations
+  has_many :invited_users, through: :invitations, source: :user
+
+  scope :past, -> { where('date < ?', Time.now) }
+  scope :upcoming, -> { where('date >= ?', Time.now) }
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
+
+  has_many :event_attendances, foreign_key: :attendee_id, dependent: :destroy
+  has_many :attended_events, through: :event_attendances, source: :attended_event
+
+  has_many :invitations
+  has_many :invited_events, through: :invitations, source: :event
 end
